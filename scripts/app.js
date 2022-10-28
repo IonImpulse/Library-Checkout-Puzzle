dce90338c270181bbd067756abf60731b1ac7ecf
Ethan Vazquez <edv121@outlook.com>
2022-10-27 17:37:57 -0700

    up

--- scripts/app.js
+++ scripts/app.js
@@ -22,12 +22,75 @@ const name_barcode_key = {
     "Percy Jackson": "9781423140623",
 }
 
+var riddles = [];
+var current_riddle = 0;
+
 function setScreen(screen) {
     let els = document.getElementsByClassName("screen");
 
     for (let i = 0; i < els.length; i++) {
         els[i].classList.add("hidden");
     }
-
+    
     document.getElementById(`${screen}-screen`).classList.remove("hidden");
+
+    if (screen == "checkout") {
+        startCheckout();
+    }
 }
+
+
+function startCheckout() {
+    const el = document.getElementById("checkout-list");
+
+    el.innerHTML = "";
+
+    riddles = Object.values(riddle_key);
+    current_riddle = 0;
+    // Shuffle the riddles
+    for (let i = riddles.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [riddles[i], riddles[j]] = [riddles[j], riddles[i]];
+    }
+
+    for (let i = 0; i < riddles.length; i++) {
+        let li = document.createElement("li");
+        li.classList.add("checkout-item");
+        // pick from riddle_key randomly
+        li.innerHTML = "Book Description: " + riddles[i];
+        el.appendChild(li);
+    }
+
+    document.getElementById("barcode-input").focus();
+}
+
+function checkout() {
+    let riddle = document.getElementById("checkout-list").children[current_riddle].innerHTML.split(": ")[1];
+    let barcode = document.getElementById("barcode-input").value;
+
+    if (barcode == name_barcode_key[Object.keys(riddle_key).find(key => riddle_key[key] === riddle)]) {
+        current_riddle++;
+        if (current_riddle == riddles.length) {
+            setScreen("end");
+        } else {
+            document.getElementById("barcode-input").value = "";
+            document.getElementById("checkout-list").children[current_riddle - 1].classList.add("checked-out");
+        }
+    } else {
+        document.getElementById("barcode-input").value = "";
+        document.getElementById("barcode-input").focus();
+        document.getElementById("barcode-input").classList.add("error");
+        startCheckout();
+        setTimeout(function() {
+            document.getElementById("barcode-input").classList.remove("error");
+        }, 1000);
+    }
+
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key == "Tab") {
+        event.preventDefault();
+        checkout();
+    }
+});
